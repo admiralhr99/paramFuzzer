fbfa39d5efbb5b416d07c981dc8a7ab5b88c0f66
admiralhr99 <admiralhr99@gmail.com>
2024-07-30 17:27:47 +0330

    Changes added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/admiralhr99/paramFuzzer
+
+go 1.21.4
+
+require golang.org/x/net v0.27.0
--- main.go
+++ main.go
@@ -2,25 +2,38 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
 	"fmt"
-	"golang.org/x/net/html"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/net/html"
 )
 
 // FetchHTML fetches HTML content from a given URL
 func FetchHTML(url string) (string, error) {
-	resp, err := http.Get(url)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
@@ -68,7 +81,7 @@ func ExtractHTMLInfo(htmlContent string) []string {
 // ExtractJSInfo extracts information from JavaScript content
 func ExtractJSInfo(jsContent string) []string {
 	var output []string
-	varNameRegex := regexp.MustCompile(`var (\w+) =`)
+	varNameRegex := regexp.MustCompile(`(?:var|let|const)\s+(\w+)\s*=`)
 	varNames := varNameRegex.FindAllStringSubmatch(jsContent, -1)
 	for _, name := range varNames {
 		output = append(output, name[1])
@@ -91,12 +104,12 @@ func main() {
 	uniqueOutput := make(map[string]struct{})
 
 	if *inputFile != "" {
-		content, err := ioutil.ReadFile(*inputFile)
+		content, err := os.ReadFile(*inputFile)
 		if err != nil {
 			fmt.Println("Error reading file:", err)
 			return
 		}
-		urls = strings.Split(string(content), "\n")
+		urls = strings.Fields(string(content))
 	} else {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
@@ -104,21 +117,36 @@ func main() {
 		}
 	}
 
-	for _, url := range urls {
-		var tempOutput []string
-		if *mode == "html" || *mode == "all" {
-			htmlContent, err := FetchHTML(url)
-			if err == nil {
-				tempOutput = append(tempOutput, ExtractHTMLInfo(htmlContent)...)
-			}
-		}
-		if *mode == "js" || *mode == "all" {
-			jsContent, err := FetchHTML(url)
-			if err == nil {
-				tempOutput = append(tempOutput, ExtractJSInfo(jsContent)...)
-			}
-		}
+	var wg sync.WaitGroup
+	resultChan := make(chan []string, len(urls))
 
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			var tempOutput []string
+			if *mode == "html" || *mode == "all" {
+				htmlContent, err := FetchHTML(url)
+				if err == nil {
+					tempOutput = append(tempOutput, ExtractHTMLInfo(htmlContent)...)
+				}
+			}
+			if *mode == "js" || *mode == "all" {
+				jsContent, err := FetchHTML(url)
+				if err == nil {
+					tempOutput = append(tempOutput, ExtractJSInfo(jsContent)...)
+				}
+			}
+			resultChan <- tempOutput
+		}(url)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	for tempOutput := range resultChan {
 		for _, item := range tempOutput {
 			uniqueOutput[item] = struct{}{}
 		}
@@ -134,7 +162,10 @@ func main() {
 
 	result := strings.Join(output, "\n")
 	if *outputFile != "" {
-		ioutil.WriteFile(*outputFile, []byte(result), 0644)
+		err := os.WriteFile(*outputFile, []byte(result), 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing to file: %v\n", err)
+		}
 	} else {
 		fmt.Println(result)
 	}
